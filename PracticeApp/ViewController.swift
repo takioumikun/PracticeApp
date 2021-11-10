405dc385c240f781682f4da8e7ca1b02d69a0131
Yuta Fujii <yutayutakid21@gmail.com>
2021-11-10 18:25:49 +0900

    Initial Commit

---
+++ PracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeApp
+//
+//  Created by Yuta Fujii on 2021/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

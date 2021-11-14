d826847a4bbfa4561dc6652eb8545ad10e5cfc4c
h-sakana <kgr.sat@gmail.com>
2021-11-14 16:19:25 +0900

    （課題）sakana

---
+++ PracticeApp/Controller/SakanaController.swift
@@ -0,0 +1,29 @@
+//
+//  SakanaController.swift
+//  PracticeApp
+//
+//  Created by h.kogure on 2021/11/14.
+//
+
+import UIKit
+
+class SakanaController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
